378d2605fff95d0063715749dd9609b9bf6b10b3
vargaa4 <vargaa4@rpi.edu>
2016-12-01 14:58:43 -0500

    Fixed computeRoundLoser and STVWinner with ties
    There was a bug with the way votes were totaled regarding ties and
    candidates who had previously dropped out. It should now be fixed.

--- prefpy/mechanismSTV.py
+++ prefpy/mechanismSTV.py
@@ -1,5 +1,6 @@
 import math
 import io
+import sys
 from mechanism import Mechanism
 from preference import Preference
 from profile import Profile
@@ -41,32 +42,53 @@ class MechanismSTV(Mechanism):
 		rankMaps = []
 		counts = [] #could use getPreferenceCounts
 		for preference in profile.preferences:
+			# check = preference.getReverseRankMap()
+			# print(check)
 			rankMaps.append(preference.getReverseRankMap())
 			counts.append(preference.count)
 
 		if (len(rankMaps) != len(counts)):
 			print("something is wrong")
-
+			
 		totals = dict()
-		for rank in rankMaps:
+		flag = 0
+		for k in range(len(rankMaps)):
+			flag = 0
             #  Ranks are listed starting from 1
+			rank = rankMaps[k]
+			# print (rank)
 			for i in range(1, len(rank) + 1):
 				# print rank[i][0]
 				# new_rank = rank[i]
-				if (rank[i][0] not in droppedOut):
-					# print totals
-					# print counts[i]
-					# print totals[rank[i][0]], counts[i]
-					new_rank = rank[i][0]
-					if (rank[i][0] in totals):
-						totals[rank[i][0]] += counts[i]
-					else:
-						totals[rank[i][0]] = counts[i]
+				for j in range(0, len(rank[i])):
+					if (rank[i][j] not in droppedOut):
+						# print totals
+						# print counts[i]
+						# print totals[rank[i][j]], counts[i]
+						new_rank = rank[i][j]
+						if (rank[i][j] in totals):
+							totals[rank[i][j]] += counts[k]
+						else:
+							totals[rank[i][j]] = counts[k]
+						flag = 1
+						# print (k)
+						# print ("rank[i][j]",rank[i][j]," total:", totals[rank[i][j]])
+				if (flag == 1):
 					break
 
-		minVotes = min(totals.values())
+					
+					
+		#	NEED TO EXCLUDE KEYS WHO HAVE BEEN DROPPED			
+		
+		voteTotals = totals.values()
+		minVotes = sys.maxsize
+		for i in range (0, len(voteTotals)):
+			print (voteTotals[i])
+			if (voteTotals[i] < minVotes):
+				minVotes = voteTotals[i]
 
 		losers = [key for key, value in totals.iteritems() if value == minVotes]
+		print(losers)
 		return losers
 
 	def STVWinner(self, profile):
@@ -81,14 +103,22 @@ class MechanismSTV(Mechanism):
 		dropouts = [[]]
 
 		for i in range(profile.numCands - 1):
-			for j in range(len(losers)):
+			j=0
+			while j <len(losers):
 				dropouts[j] = self.computeRoundLoser(profile, losers[j])
-				print dropouts[j]
-				losers[j].append(dropouts[j][0])
-				for k in range(1, len(dropouts[j]), 1):
-					losers.append(list(losers[j]))
-					losers[-1].append(dropouts[j][k])
-					dropouts.append([])
+				# print ("Losers:", losers);
+				# print ("Dropouts:",dropouts)
+				print
+				if (dropouts[j]):
+					losers[j].append(dropouts[j][0])
+					for k in range(1, len(dropouts[j])):
+						# print ("j:",j);
+						losers.append(list(losers[j]))
+						losers[-1].pop() 						#we want this so we only add one element at a time to losers
+						losers[-1].append(dropouts[j][k])
+						dropouts.append([])
+				# dropouts[j] = []
+				j+=1
 
 		# Now we should have a list of lists called 'losers' which contains all losers for different tiebreaks
 
@@ -111,4 +141,4 @@ if __name__ == "__main__":
 
 	m = MechanismSTV()
 
-	print m.STVWinner(p)
+	print (m.STVWinner(p))

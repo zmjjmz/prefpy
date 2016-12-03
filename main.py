98b5bd93cf7fee578617d1e1cc2222d3af860874
Levin Huang <lpotato4@gmail.com>
2016-12-03 17:04:59 -0500

    added edge preference functionality, more comments, pruned redundant code, and added two testcases.

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ if __name__ == '__main__' :
 	data=Profile({},[])
 
 	#data.importPreflibFile("4candpartialorder.txt")
-	data.importPreflibFile("input0")
+	data.importPreflibFile("input1.txt")
 	
 	rankpairMech=RankedPairs()
 	myList = [(4, 'a', 'b'), (3, 'b', 'c'), (3, 'c', 'd'), (2, 'd', 'b'), (2, 'c', 'a'), (4, 'd', 'a')]
+	myEdgePrefList = [('a', 'b'), ('a', 'c'), ('b', 'c'), ('c', 'b'), ('b', 'a'), ('c', 'a'), ]
 	#print(rankpairMech.getWinners(edges = myList))
+	#print(rankpairMech.getWinners(prof = data, edgePrefList = myEdgePrefList))
 	print(rankpairMech.getWinners(prof = data))
-    #print(rankpairMech.getOneWinner(data))
-    #edgeList=rankpairMech.getSortedEdges(data)
-    #rankpairMech.createNXGraph(edgeList)
+	#print(rankpairMech.getOneWinner(prof = data))
--- prefpy/rankedPairs.py
+++ prefpy/rankedPairs.py
@@ -1,8 +1,6 @@
 '''
 Authors: Bilal Salam, Levin Huang, Lucky Cho
 '''
-
-
 import io
 import math
 import itertools
@@ -13,7 +11,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import copy
 
-def getTopRank(netxGraph):
+def getWinningNodes(netxGraph):
 	#function to find the winning nodes given a networkx graph
 	topRanks = []
 	for i in netxGraph.nodes():
@@ -32,12 +30,14 @@ class branchedGraph():
 			return True
 		else:
 			return False
-			
-	def getNextEdge(self, currentWinners):
+	
+	#currentWinners - list of string names of known winners
+	#edgePrefList - [(node1, node2), (node2, node3)] list of edges sorted in decreasing order of preference, each tuple is an edge 
+	def getNextEdge(self, currentWinners, edgePrefList = None):
 	#function to progress through each 
 		#list of graphs to branch to
 		branchedGraphList = [] 
-		branchedEdges, branchedRemEdges = self.findTies()
+		branchedEdges, branchedRemEdges = self.findTies(edgePrefList)
 		#iterate through each branch of the graph
 		for i in range(len(branchedEdges)):
 			tmpGraph = self.DG.copy()
@@ -45,7 +45,7 @@ class branchedGraph():
 			#add the edge
 			self.addOneWayEdge(tmpGraph, tmpEdge)
 			#pruning check to see if we have already found the possible winners
-			if not set(getTopRank(tmpGraph)).issubset(set(currentWinners)):
+			if not set(getWinningNodes(tmpGraph)).issubset(set(currentWinners)):
 				tmpBranchedGraph = branchedGraph(branchedRemEdges[i], tmpGraph)
 				#add a new branched graph to the branchedGraphList
 				branchedGraphList.append( tmpBranchedGraph)
@@ -61,40 +61,50 @@ class branchedGraph():
 			except:
 				pass
 
-	def findTies(self):
+	#edgePrefList - [(node1, node2), (node2, node3)] list of edges sorted in decreasing order of preference, each tuple is an edge 
+	def findTies(self, edgePrefList = None):
 	#function to find which edges are tied
 		branchedRemEdges = [] #list of lists
 		edges = self.remainingList 
-		tmpList = [] #holds list of edges to branch out to
+		tmpList = [] #holds list of tied edges to branch out to
 		tmpRemEdges = []
+		
 		if len(edges) > 0: #we know that we don't care about negative edge weights
 			tmpWeight = edges[0][0]
 			#loop through the list of edges
 			for i in range(len(edges)):
 				if edges[i][0] == tmpWeight: #we find tied edge that we want to use
-					tmpRemEdges = copy.copy(edges) #deep copy edge list
-					del tmpRemEdges[i] #remove tied edge from the remaining list
-					branchedRemEdges.append(tmpRemEdges)
 					tmpList.append(copy.copy(edges[i])) #add the edge we are using to the list of edges to extend G`
+		if edgePrefList is not None:
+			if len(tmpList) > 0:
+				tmpList = self.getBestEdge(tmpList, edgePrefList)
+		for i in range(len(edges)):
+			tmpRemEdges = copy.copy(edges) #deep copy edge list
+			del tmpRemEdges[i] #remove tied edge from the remaining list
+			branchedRemEdges.append(tmpRemEdges)
 		return tmpList, branchedRemEdges
+	
+	def getBestEdge(self, edgeList, edgePrefList):
+		bestIndex = None
+		edgeListIndex = 0
+		for i in range(0, len(edgeList)):
+			edge = edgeList[i]
+			try:
+				tmpBestIndex = edgePrefList.index((edge[1], edge[2]))
+				if bestIndex is None or bestIndex > tmpBestIndex:
+					bestIndex = tmpBestIndex
+					edgeListIndex = i
+			except ValueError:
+				print('edge does not exist in preference list, user did not give preference over all edges')
+		return [edgeList[edgeListIndex]]
+				
+			
+	
 	
 
 class RankedPairs(Mechanism):
-	def __init__(self):
-		pass
-	#returns a list of tuples(weight,label,label)
-	def createNXGraph(self,edgeList):
-		DG = nx.DiGraph()
-		for edge in edgeList:
-			DG.add_edge(edge[1], edge[2], weight=edge[0])
-		#nx.draw(DG)
-		#stuff for drawing the graph
-		nodeLayout=nx.spring_layout(DG)
-		nx.draw_networkx(DG,pos=nodeLayout,arrows=True)
-		labels = nx.get_edge_attributes(DG, 'weight')
-		nx.draw_networkx_edge_labels(DG, pos=nodeLayout, edge_labels=labels)
-		plt.show()
-		
+#Calculates winners using the RankedPairs voting mechanism
+
 	#returns edges from the wmg sorted by weights
 	def getSortedEdges(self,prof):
 		#initialize the wmg from the given profile
@@ -118,57 +128,33 @@ class RankedPairs(Mechanism):
 		labels = nx.get_edge_attributes(DG, 'weight')
 		nx.draw_networkx_edge_labels(DG, pos=nodeLayout, edge_labels=labels)
 		#plt.show()
-		
-		
-	def getNewGraph(self,prof):
-		#function to get the wmg and netx graph from the profile for the getOneWinner function
-		wmg=prof.getWmg()
-		DG=nx.DiGraph()
-		newGraph=nx.DiGraph()
-		edges=[]
-		#initialize new graph to all zero edges
-		edges=self.getSortedEdges(prof)
-		for edge in edges:
-			if edge[0]<0:
-				break
-			DG.add_edge(edge[1], edge[2], weight=edge[0])
-			try:
-				#if there is no cycle the method throws an exception
-				nx.find_cycle(DG)
-				DG.remove_edge(edge[1], edge[2])
-				break
-				# every edge is initialized to 0
-				# may have to delete edge instead
-			except:
-				pass
-		return DG
 
-	def getOneWinner(self,prof):
+	def getOneWinner(self, prof = None, edgePrefList = None):
 	#function to get one winner
-		newGraph=self.getNewGraph(prof)
-		numCanidates=newGraph.number_of_nodes()
-		winners = []
-		for i in newGraph.nodes():
-			inedges = newGraph.in_edges(i)
-			if inedges == []:
-				winners.append(i)
-		return winners
-
-	def getWinnerWithTieBreakingMech(self,prof,prefList):
-		pass
+		edges = self.getSortedEdges(prof)
+		DG=self.initDG(edges)#get the networkx Directed Graph from the set of edges
+		winners = [] #list to hold the winners
+		doneList = [] #list of the graphs that have been completed
+		newBranchedGraph = branchedGraph(edges, DG) #create the first branched graph
+		graphs = newBranchedGraph.getNextEdge(winners, edgePrefList) #try to get the next edge from the new branched graph
 		
-	def getTopRank(self, graphList):
-	#function to get the winners from the donelist
-		topRanks = []
-		for bGraph in graphList:
-			newGraph = bGraph.DG
-			for i in newGraph.nodes():
-				#check if the node has any in-edges
-				inedges = newGraph.in_edges(i)
-				#if it has no in-edges, then we know that it is a winner
-				if inedges == []:
-					topRanks.append(i)
-		return topRanks
+		while(len(graphs) != 0):
+		#Iterating through graphs, appending to tmp graphs list such that we arent modifying the list we are iterating over
+			tmpGraphs = []
+			graphs = graphs[0:1]
+			graph=graphs.pop(0) #we've finished looking at this graph
+			tmpNextEdgeList = graph.getNextEdge(winners, edgePrefList) #look at the next edge of the graph
+			if tmpNextEdgeList == [] and graph.isDone(): #we know that the graph is complete if it satisfies these conditions
+				doneList.append(graph)
+				winners += getWinningNodes(graph.DG) #add the nodes returned by the getWinningNodes call
+			else:
+				graphs=tmpNextEdgeList+graphs
+		print(len(doneList)) #debug statement to see how many graphs needed to be computed
+		#loop to output each completed graph
+		for winner in doneList:
+			self.drawGraph(winner.DG)
+		plt.show()
+		return set(winners)
 		
 	def initDG(self, edges):
 	#function to intialize the directed graph
@@ -179,43 +165,36 @@ class RankedPairs(Mechanism):
 		DG.add_nodes_from(nodeSet)
 		return DG
 
-	def getWinners(self, prof=None, edges=None):
+	def getWinners(self, prof=None, edgePrefList=None):
 	#primary method to calculate multiple winners
-		if edges is None:
-			edges = self.getSortedEdges(prof)
-		else:
-			edges = sorted(edges, key=lambda weight: weight[0], reverse = True)
-		print(edges)
-		#DG = nx.DiGraph()
+		edges = self.getSortedEdges(prof)
 		
-		#edges = self.getSortEdges(prof) #create the array of edges
 		DG=self.initDG(edges)#get the networkx Directed Graph from the set of edges
-
 		winners = [] #list to hold the winners
 		doneList = [] #list of the graphs that have been completed
 		newBranchedGraph = branchedGraph(edges, DG) #create the first branched graph
-		graphs = newBranchedGraph.getNextEdge(winners) #try to get the next edge from the new branched graph
+		graphs = newBranchedGraph.getNextEdge(winners, edgePrefList) #try to get the next edge from the new branched graph
+		
 		while(len(graphs) != 0):
 		#Iterating through graphs, appending to tmp graphs list such that we arent modifying the list we are iterating over
 			tmpGraphs = []
 			graph=graphs.pop(0) #we've finished looking at this graph
-			tmpNextEdgeList = graph.getNextEdge(winners) #look at the next edge of the graph
+			tmpNextEdgeList = graph.getNextEdge(winners, edgePrefList) #look at the next edge of the graph
 			if tmpNextEdgeList == [] and graph.isDone(): #we know that the graph is complete if it satisfies these conditions
 				doneList.append(graph)
-				winners += self.getTopRank(doneList) #add the nodes returned by the getTopRank call
+				winners += getWinningNodes(graph.DG) #add the nodes returned by the getWinningNodes call
 			else:
 				graphs=tmpNextEdgeList+graphs
 		print(len(doneList)) #debug statement to see how many graphs needed to be computed
+		'''
 		#loop to output each completed graph
 		for winner in doneList:
 			self.drawGraph(winner.DG)
-		plt.show()
+		plt.show()'''
 		
-		return set(winners)
-
-
-	def getRanking(self, prof):
-		pass
+		return set(winners), [graph.DG for graph in doneList]
+		#returns the set of winning nodes and then the linear orders justifying each winner in the form of networkx objects
+		#uncomment the above loop for a visual representation
 
 
 
